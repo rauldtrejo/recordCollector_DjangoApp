3def2259795db4411d217e799800580ebac68b0b
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-03-08 17:04:52 -0800

    Added Record Model and migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Record(models.Model):
+    name = models.CharField(max_length=100)
+    artist = models.CharField( max_length=100)
+    format = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    genre = models.CharField(max_length=100)
+    released = models.CharField(max_length=100)
+
