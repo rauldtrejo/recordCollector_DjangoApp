2234ad773284cd99719f36e4fb25e1061b5eab30
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-03-08 22:18:47 -0800

    Added record_details view

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Record
 # Create your views here.
 
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -12,3 +12,7 @@ def about(request):
 def records_index(request):
   records = Record.objects.all()
   return render(request, 'records/index.html', { 'records':records})
+
+def records_detail(request, record_id):
+  record = Record.objects.get(id=record_id)
+  return render(request, 'records/detail.html', {'record':record})
